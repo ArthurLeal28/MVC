10bed2a0507f7c5478334f65d65565d1bb9dd655
Arthur Carvalho Leal da Fonseca <86314583+ArthurLeal28@users.noreply.github.com>
2021-12-13 09:19:41 -0300

    Add files via upload

---
+++ Programa.java
@@ -0,0 +1,16 @@
+
+package sistema;
+
+import sistema.controller.Sistema;
+
+public class Programa {
+
+	public static void main(String[] args) {
+//		Sistema sis = Sistema.getInstance();
+//		sis.init();
+		Sistema.getInstance().init();
+		//menu();
+	}
+
+	
+}
